034e85387194ca97870dc90e56997bfe13edaf11
Sebastian Isufaj <seb.encod@gmail.com>
2022-11-29 10:42:27 +0100

    Create data-types.js

---
+++ data-types.js
@@ -0,0 +1,25 @@
+console.log(("---------------Numbers--------------------\n"));
+
+let numberOfDays = 30 ///
+
+console.log("google" / 5);
+
+var loku = 25;
+var loku = 30;
+console.log(loku)
+
+
+let thisISAString = "hello";
+let thisIsAnotherString = "there";
+let thisIsYetAnotherString = "you";
+
+let aLongString = thisISAString + thisIsAnotherString;
+
+console.log(thisISAString);
+console.log(thisIsAnotherString);
+console.log(thisIsYetAnotherString);
+
+console.log(aLongString);
+console.log(typeof thisISAString);
+
+v
